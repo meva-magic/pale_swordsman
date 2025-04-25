72a02c10461d69d76d211dfff22dc6531dba1791
saharov-dev <saharov.dev@gmail.com>
2025-04-25 19:43:36 +0300

    shaky text

---
+++ pale swordsman/Assets/Scripts/TextShake.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using TMPro;
+
+public class TextShake : MonoBehaviour
+{
+    [SerializeField] private TMP_Text textComponent;
+
+    void Update()
+    {
+        textComponent.ForceMeshUpdate();
+        var textInfo = textComponent.textInfo;
+
+        for (int i = 0; i < textInfo.characterCount; ++i)
+        {
+            var charInfo = textInfo.characterInfo[i];
+
+            if (!charInfo.isVisible)
+            {
+                continue;
+            }
+
+            var verts = textInfo.meshInfo[charInfo.materialReferenceIndex].vertices;
+
+            for (int j = 0; j < 2; ++j)
+            {
+                var orig = verts[charInfo.vertexIndex + j];
+                verts[charInfo.vertexIndex + j] = orig + new Vector3(Mathf.Sin(Time.time * 40f + orig.x * 0.01f) * 10f, Mathf.Sin(Time.time * 40f + orig.x * 0.01f) * 10f, 0);
+            }
+        }
+
+        for (int i = 0; i < textInfo.meshInfo.Length; ++i)
+        {
+            var meshInfo = textInfo.meshInfo[i];
+            meshInfo.mesh.vertices = meshInfo.vertices;
+            textComponent.UpdateGeometry(meshInfo.mesh, i);
+        }
+    }
+}
